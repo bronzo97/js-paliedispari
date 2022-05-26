904d5d8bf6e42ee22da3952376fab927aab32c4e
bronzo97 <danilobronzo97@hotmail.it>
2022-05-26 16:38:10 +0200

    palindrome exercise done

--- Palindroma/js/app.js
+++ Palindroma/js/app.js
@@ -1,20 +1,40 @@
 
+let wordReverse = [];
+
+let word = prompt('Dimmi una parola');
 
 
-// function palindrome(word, ) {
-    const word = prompt('Dimmi una parola');
+function palindrome() {
+
     let wordDecomposed = word.split("");
     console.log(wordDecomposed);
 
-    for(i = 0 ; i < wordDecomposed.length; i++) {
-       
+    for(let i = word.length - 1; i >= 0; i--) {
+        wordReverse.push(wordDecomposed[i]);
     }
-// }
+
+    console.log(wordReverse);
+
+    let wordReverseJoin = wordReverse.join('');
+    
+    if (word === wordReverseJoin) {
+        console.log('è palindroma');
+    } else {
+        console.log('non è palindroma');
+
+    }
+    
+}
+
+console.log(palindrome());
+
+
+// console.log(palindrome);
 
 // chiedere la parola all'utante
 // salvare la parola
 // scomporre la parola
 // leggere la parola al contrario
-// creare un arrai con la parola al contrario
+// creare un array con la parola al contrario
 // controllare se parola normale è uguale alla parola rovesciata
 // mostrare il risultato
