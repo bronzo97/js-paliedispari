68821dd689f54e4dae624ace5a499a88cea0d7e4
bronzo97 <danilobronzo97@hotmail.it>
2022-05-26 15:44:12 +0200

    created repository

---
+++ Palindroma/js/app.js
@@ -0,0 +1,20 @@
+
+
+
+// function palindrome(word, ) {
+    const word = prompt('Dimmi una parola');
+    let wordDecomposed = word.split("");
+    console.log(wordDecomposed);
+
+    for(i = 0 ; i < wordDecomposed.length; i++) {
+       
+    }
+// }
+
+// chiedere la parola all'utante
+// salvare la parola
+// scomporre la parola
+// leggere la parola al contrario
+// creare un arrai con la parola al contrario
+// controllare se parola normale è uguale alla parola rovesciata
+// mostrare il risultato
